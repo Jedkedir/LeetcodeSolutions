9c15562768a35c1dda9fc744ed92d83c915a9b50
Yididiya Kedir <149969175+Jedkedir@users.noreply.github.com>
2026-01-29 21:50:52 +0300

    Time: 3 ms (26.64%) | Memory: 19.4 MB (30.38%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -3,7 +3,7 @@ class Solution:
         ans = ""
         for x in zip(*strs):
             if len(set(x)) == 1:
-                ans = ans+x[0]
+                ans = ans + x[0]
             else:
                 break
         return ans
