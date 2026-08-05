0f5a482595230cb6c1cfaaa766a41a89f4e1c4d2
Yididiya Kedir <149969175+Jedkedir@users.noreply.github.com>
2026-08-05 21:08:06 +0300

    Time: 283 ms (71.97%) | Memory: 106.9 MB (59.85%) - LeetSync

---
+++ 3561-remove-methods-from-project/remove-methods-from-project.py
@@ -0,0 +1,21 @@
+class Solution:
+    def remainingMethods(self, n: int, k: int, invocations: List[List[int]]) -> List[int]:
+        graph = defaultdict(list)
+        for u, v in invocations:
+            graph[u].append(v)
+            
+        suspicious = set()
+        stack = [k]
+        while stack:
+            curr = stack.pop()
+            if curr not in suspicious:
+                suspicious.add(curr)
+                for neighbor in graph[curr]:
+                    if neighbor not in suspicious:
+                        stack.append(neighbor)
+                        
+        for u, v in invocations:
+            if u not in suspicious and v in suspicious:
+                return list(range(n))
+                
+        return [i for i in range(n) if i not in suspicious]
